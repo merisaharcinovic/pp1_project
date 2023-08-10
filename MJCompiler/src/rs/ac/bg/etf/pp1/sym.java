0c457fdab8774f46ee923eb71ffc688fbf1b5a9d
Merisa Harčinović <102028878+merisaharcinovic@users.noreply.github.com>
2023-08-10 15:08:52 +0200

    initial commit, .lex done

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/sym.java
@@ -0,0 +1,58 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym {
+
+	public static final int PROG = 1;
+	public static final int BREAK = 2;
+	public static final int CLASS = 3;
+	public static final int ELSE = 4;
+	public static final int CONST = 5;
+	public static final int IF = 6;
+	public static final int WHILE = 7;
+	public static final int NEW = 8;
+	public static final int PRINT = 9;
+	public static final int READ = 10;
+	public static final int RETURN = 11;
+	public static final int VOID = 12;
+	public static final int EXTENDS = 13;
+	public static final int CONTINUE = 14;
+	public static final int FOREACH = 15;
+	public static final int FINDANY = 16;
+
+	public static final int PLUS = 17;
+	public static final int MINUS = 18;
+	public static final int MUL = 19;
+	public static final int DIV = 20;
+	public static final int MOD = 21;
+	public static final int LAMBDA = 22;
+	public static final int EQUAL = 23;
+	public static final int NOT_EQUAL = 24;
+	public static final int GREATER_EQUAL = 25;
+	public static final int LESS_EQUAL = 26;
+	public static final int AND = 27;
+	public static final int OR = 28;
+	public static final int GREATER = 29;
+	public static final int LESS = 30;
+	public static final int ASSIGNMENT = 31;
+	public static final int INC = 32;
+	public static final int DEC = 33;
+	public static final int SEMICOLON = 34;
+	public static final int COLON = 35;
+	public static final int COMMA = 36;
+	public static final int DOT = 37;
+	public static final int LPAREN = 38;
+	public static final int RPAREN = 39;
+	public static final int LSQUARE = 40;
+	public static final int RSQUARE = 41;
+	public static final int LBRACE = 42;
+	public static final int RBRACE = 43;
+
+	public static final int CHAR_CONST = 44;
+	public static final int BOOL_CONST = 45;
+	public static final int NUMBER = 46;
+
+	public static final int IDENT = 47;
+
+	public static final int EOF = 48;
+
+}
