50fdc5bc11d6cbcc7e6c7fa59b30ff93326a1d68
Merisa Harčinović <102028878+merisaharcinovic@users.noreply.github.com>
2023-08-17 18:17:54 +0200

    f3: constDecl, methodName, methodDecl

--- MJCompiler/src/rs/ac/bg/etf/pp1/pp1/SemanticAnalyzer.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/pp1/SemanticAnalyzer.java
@@ -12,6 +12,7 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 	int printCount = 0;
 	int varDeclCount = 0;
 	boolean errorDetected = false;
+	boolean hasMain=false;
 	
 	Struct currentType = null;
 
@@ -35,29 +36,7 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 		log.info(msg.toString());
 	}
 
-	public void visit(VarOneVar oneVar){
-		varDeclCount++;
-		
-		Obj foundVar = Tab.find(oneVar.getName());
-    	if(foundVar == Tab.noObj){
-    		Obj varNode = Tab.insert(Obj.Var, oneVar.getName(), currentType);
-    	}else{
-    		report_error("Greska: Ime " + oneVar.getName() + " je vec deklarisano!", oneVar);	
-    	}
-		
-	}
 	
-	public void visit(VarOneArray arrayVar){
-		varDeclCount++;
-		
-		Obj foundVar = Tab.find(arrayVar.getName());
-    	if(foundVar == Tab.noObj){
-    		Obj varNode = Tab.insert(Obj.Var, arrayVar.getName(), new Struct(Struct.Array, currentType));
-    	}else{
-    		report_error("Greska: Ime " + arrayVar.getName() + " je vec deklarisano!", arrayVar);	
-    	}
-		
-	}
 	
     public void visit(StatementPrint print) {
 		printCount++;
@@ -69,6 +48,8 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 	}
     
     public void visit(Program program) {
+    	if(!hasMain) report_error("Greska: Nije pronadjena main metoda. ", null);
+
     	Tab.chainLocalSymbols(program.getProgName().obj);
     	Tab.closeScope();
 	}
@@ -89,6 +70,119 @@ public class SemanticAnalyzer extends VisitorAdaptor {
     		}
     	}
     }
+    
+    public void visit(VarOneVar oneVar){
+		varDeclCount++;
+		
+		Obj foundVar = Tab.find(oneVar.getName());
+    	if(foundVar == Tab.noObj){
+    		Obj varNode = Tab.insert(Obj.Var, oneVar.getName(), currentType);
+    		report_info("Deklarisana promenljiva: " + oneVar.getName(), oneVar);
+    	}else{
+    		report_error("Greska: Ime " + oneVar.getName() + " je vec deklarisano!", oneVar);	
+    	}
+		
+	}
+	
+	public void visit(VarOneArray arrayVar){
+		varDeclCount++;
+		
+		Obj foundVar = Tab.find(arrayVar.getName());
+    	if(foundVar == Tab.noObj){
+    		Obj varNode = Tab.insert(Obj.Var, arrayVar.getName(), new Struct(Struct.Array, currentType));
+    		report_info("Deklarisana promenljiva: " + arrayVar.getName(), arrayVar);
+
+    	}else{
+    		report_error("Greska: Ime " + arrayVar.getName() + " je vec deklarisano!", arrayVar);	
+    	}
+		
+	}
+    
+	//deklaracija CONST 
+	
+    public void visit(ConstNumber constNumber) {
+    	Obj found = Tab.find(constNumber.getName());
+    	
+    	if(found == Tab.noObj){
+    		if(currentType.getKind() !=  Struct.Int){
+        		report_error("Greska: Konstanta " + constNumber.getName() + " nije ispravnog tipa!", constNumber);	
+    		}
+    		else {
+    			//insert obj cvor u tab i postavi vrednost value u adr
+        		Obj constNumNode = Tab.insert(Obj.Con, constNumber.getName(), Tab.intType);
+        		constNumNode.setAdr(constNumber.getValue());
+        		report_info("Deklarisana NUMBER konstanta: " + constNumber.getName() + "= " + constNumber.getValue(), constNumber);
+
+    		}
+    	}else{
+    		report_error("Greska: Ime " + constNumber.getName() + " je vec deklarisano!", constNumber);	
+    	}
+	}
+    
+    
+    public void visit(ConstChar constChar) {
+    	Obj found = Tab.find(constChar.getName());
+    	
+    	if(found == Tab.noObj){
+    		if(currentType.getKind() !=  Struct.Char){
+        		report_error("Greska: Konstanta " + constChar.getName() + " nije ispravnog tipa!", constChar);	
+    		}
+    		else {
+        		Obj constCharNode = Tab.insert(Obj.Con, constChar.getName(), Tab.charType);
+        		constCharNode.setAdr(constChar.getValue());
+        		report_info("Deklarisana CHAR konstanta: " + constChar.getName() + "= " + constChar.getValue(), constChar);
+
+    		}
+    	}else{
+    		report_error("Greska: Ime " + constChar.getName() + " je vec deklarisano!", constChar);	
+    	}
+	}
+    
+    public void visit(ConstBool constBool) {
+    	Obj found = Tab.find(constBool.getName());
+    	
+    	if(found == Tab.noObj){
+    		if(currentType.getKind() !=  Struct.Bool){
+        		report_error("Greska: Konstanta " + constBool.getName() + " nije ispravnog tipa!", constBool);	
+    		}
+    		else {
+        		Obj constBoolNode = Tab.insert(Obj.Con, constBool.getName(), Tab.intType);
+        		int value = constBool.getValue().equals("true") ? 1 : 0;
+        		constBoolNode.setAdr(value);
+        		report_info("Deklarisana BOOLEAN konstanta: " + constBool.getName() + "= " + constBool.getValue(), constBool);
+
+    		}
+    	}else{
+    		report_error("Greska: Ime " + constBool.getName() + " je vec deklarisano!", constBool);	
+    	}
+	}
+    
+    
+    //methodDecl
+    
+    public void visit(MethodDecl methodDecl){
+		varDeclCount++;
+		
+		Tab.chainLocalSymbols(methodDecl.getMethodName().obj);
+    	Tab.closeScope();
+	}
+    
+    public void visit(MethodName methodName){
+		varDeclCount++;
+		
+		String name = methodName.getName();
+    	if(name.equals("main")){
+    		hasMain=true;
+    		methodName.obj = Tab.insert(Obj.Meth, methodName.getName(), Tab.noType);
+        	Tab.openScope();
+    		
+    	}else{
+    		report_error("Greska: Identifikator " + methodName.getName() + " nije main!", methodName);	
+    	}
+		
+	}
+    
+    
     
     
     
